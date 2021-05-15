aae875f95fbe0924f553741e2a13c552c88ec022
leonardomv <leonardo.vegas@gmail.com>
2021-05-15 06:06:06 -0300

    Control de seleccion de contenedor y lista de procesos asociados a ese contenedor.

--- src/app/button-toggle-overview-example.ts
+++ src/app/button-toggle-overview-example.ts
@@ -1,14 +1,25 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 /**
  * @title Basic button-toggles
  */
 @Component({
   selector: 'button-toggle-overview-example',
-  templateUrl: 'button-toggle-overview-example.html',
+  templateUrl: 'button-toggle-overview-example.html'
 })
-export class ButtonToggleOverviewExample {}
+export class ButtonToggleOverviewExample {
+  contenedorSeleccionControl = new FormControl();
+  activarListaProcesos: boolean = false;
+  CocoLavadora = {
+    estado: 'Durmiendo'
+  };
 
+  seleccion(contenedor: any) {
+    this.activarListaProcesos = true;
+    console.log('Contenedor Seleccionado: ' + contenedor);
+  }
+}
 
 /**  Copyright 2021 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
